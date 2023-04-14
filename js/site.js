39ee29f71f872102c25f43e2f476e3cd6a80d61f
Angelena Nunez <angelenacorral@gmail.com>
2023-04-14 00:22:57 -0600

    finished styling and JS, except for how to associate css classes with JS  displayNumbers function

--- js/site.js
+++ js/site.js
@@ -1,67 +1,111 @@
 // get the start and end numbers from the page
 // Entry point of application AKA Controller function
-function fizz() {
-    // declared that start/endValue string is to pull and "hold" 
-    // the values from the HTMl ID "fizzBuzzNumber, fizz, buzz"
-        let fizzBuzzNumber = document.getElementById('fizzBuzzNumber').value; // fetch fizzBuzzNumber from user/HTML
-        let fizz = document.getElementById('fizz').value; // fetch fizz from user/HTML
-        let buzz = document.getElementById('buzz').value; // fetch fizz from user/HTML
-    // convert value from string to integer/value ex: startNumber/endNumber
-        let fizzBuzzNumber = parseInt(fizzBuzzNuber); 
-        let fizz = parseInt(fizz); 
-        let buzz = parseInt(buzz); 
-    /* they are valid numbers - not "NaN"*/
-    // if start is an integer it's true, 
-    // if it's not a number, it's false
-        if (Number.isInteger(startNumber) && Number.isInteger(endNumber)) {
-            // array created from startNumber/endNumber, values stored inside array numberArray
-        let numberArray = generateNumbers(startNumber, endNumber);
-        // displaysNumbers displays numberArray values
-            displayNumbers(numberArray);
+function getValues() {
+    // declared that below variables are to to pull and "hold" string values from HTML user input
+    let sodaNumber = document.getElementById('sodaNumber').value; // ex: sodaNumber = '3' from user/HTML
+    let popNumber = document.getElementById('popNumber').value; // ex: popNumber = '5' from user/HTML
+    let userNumber = document.getElementById('userNumber').value; // ex: userNumber = '45' from user input/HTML 
+    // convert values from strings to integers ex: userNumber = '45' to sodaNum = 45
+    let sodaNum = parseInt(sodaNumber);
+    let popNum = parseInt(popNumber);
+    let userNum = parseInt(userNumber);
+    // checks that all number values given by user are valid integers,
+    if (Number.isInteger(sodaNum) && Number.isInteger(popNum) && Number.isInteger(userNum)) {
+        // if variables sodaNum, popNum,and userNum are integers, it's true, 
+        // and will generate numbers and store them in array 'numberArray'
+        let numberArray = generateNumbers(sodaNum, popNum, userNum);
+        // if it's not a number (NaN), it's false, will display error msg
+        displayNumbers(numberArray);
+    } else {
+        // display an error
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops!',
+            text: 'Please enter valid numbers for the 'Soda', 'Pop', and 'Soda Pop' values.',
+            backdrop: false
+        });
+    }
+
+}
+// generate the range of numbers to display
+// Business/logic function
+function generateNumbers(sodaNum, popNum, userNum) {
+    // declare empty new array 'basketOfNumbers' 
+    // to hold values for use in the for loop below
+    let basketOfNumbers = [];
+    // for loop starts at integer at index position 1
+    // since 'userNum' is at index position 0, and already
+    // in use and accounted for inside the for loop,
+    // for loop keeps running until the conditions 
+    // of the else if statement below are met
+    for (let i = 1; i <= userNum; i = i + 1) {
+        // if the current index in array basketOfNumbers 
+        // is divisible by sodaNum & popNum,
+        // the index integer is removed from the array
+        // and replaced with the string 'Soda Pop'
+        if (i % sodaNum == 0 && i % popNum == 0) {
+            basketOfNumbers.push('Soda Pop');
+            // if the current index in array basketOfNumbers 
+            // is divisible by sodaNum,the index integer
+            //  is removed from the array and
+            //  replaced with the string 'Soda' 
+        } else if (i % sodaNum == 0) { 
+            basketOfNumbers.push('Soda');
+            // if the current index in array basketOfNumbers 
+            // is divisible by popNum,the index integer
+            // is removed from the array and
+            // replaced with the string 'Pop'
+        } else if (i % popNum == 0) {
+            basketOfNumbers.push('Pop');
+            // otherwise it replaces the integer with 
+            // itself, integer is unchanged in array
         } else {
-            //display an error
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops!',
-                text: 'Please enter valid numbers for the start and end values.'
-            });
+            basketOfNumbers.push(i);
         }
     }
-    // generate the range of numbers to display
-    // Business/logic function
-    function generateNumbers(start, end) {
-    // basketOfNumbers will accept/hold values from for loop
-        let basketOfNumbers = [];
-        // for loop starts at integer number; 
-        // keep running as long as (it's less than or equal to endNumber); 
-        // start at index number and add plus 1 each time 
-        // until previous <= statement is no longer true
-        for (let number = start; number <= end; number = number + 1) {
-            number;
-            basketOfNumbers.push(number); //[0, 2, 3,..., 100]
-        }
-    // returns or passes back out values from basketOfNumbers
-        return basketOfNumbers; // => [0, 2, 3,..., 100]
-    }
-    
-    // placing the generated numbers  on the page AND bold coloring the even numbers
-    // View function
-    function displayNumbers(numbers) { //[0, 2, 3,..., 100] => length = example: 101 digits or spaces
-        let results = '';
+    // since for loop is no longer running, the
+    // return statement immediately exits a function, 
+    // returning or passing back the value of the 
+    // expression that follows to be stored in
+    // the array 'basketOfNumbers'
+    return basketOfNumbers; // => [0, 2, 3,..., 100]
+}
+
+// placing the generated numbers  on the page AND bold coloring the even numbers
+// View function
+function displayNumbers(numbers) { //[0, 2, 3,..., 100] => length = example: 101 digits or spaces
+    let results = '';
     // for loop adds 1 to index number, then calculates if index numner is divisible by 2,
     // if remainder equals "0" exactly, ifna match it applies the CSS class of "evenNumber" 
     // coloring the even integers,else/otherwise it returns the (odd) integers with normal 
     // table row styling. 
-        for (let index = 0; index < numbers.length; index = index + 1) {
-            let currentNumber = numbers[index];
-            if (currentNumber % 2 == 0) {
-                results = results + `<tr><td class="evenNumber">${currentNumber}</td></tr>`;
-            } else {
+    for (let index = 0; index < numbers.length; index = index + 1) {
+        let currentNumber = numbers[index];
+        // if 'if' condition is met, below css class styling
+        // is applied to table data
+        if /*(currentNumber === )*/ {
+            // results = results + `<tr><td class="soda-pop">${currentNumber}</td></tr>`;
             results = results + `<tr><td>${currentNumber}</td></tr>`;
-            }
+            // if 'else if' condition is met, below css class styling
+            // is applied to table data
+        } else if /*(currentNumber === )*/ {
+            // results = results + `<tr><td class="soda">${currentNumber}</td></tr>`;
+			results = results + `<tr><td>${currentNumber}</td></tr>`;
+		}
+            // if 'if' condition is met, below css class styling
+            // is applied to table data
+        else if /*(currentNumber === )*/ {
+            // results = results + `<tr><td class="pop">${currentNumber}</td></tr>`;
+			results = results + "<tr><td>" + currentNumber + "</td></tr>";
+            // otherwise, below css class styling
+            // is applied to table data
+		} else {
+            results = results + `<tr><td>${currentNumber}</td></tr>`;
         }
-    // results = "<tr><td>10</tr></td>11<tr><td>12</tr></td>..."
-    // results are passed back into HTML ID "results" into table
-        let tableBody = document.getElementById('results');
-        tableBody.innerHTML = results;
     }
+    // results = "<tr><td>10</tr></td>11<tr><td>12</tr></td>..."
+    // results are passed back into HTML ID "results" 
+    // and displayed inside the table
+    let tableBody = document.getElementById('results');
+    tableBody.innerHTML = results;
+}
